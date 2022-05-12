a808ff38a54ef64a8ea7d5386ec616a8ea4d00c4
Rajat Raghuwanshi <77507423+rajat10cube@users.noreply.github.com>
2022-05-11 22:24:50 -0400

    register

--- js/loginscript.js
+++ js/loginscript.js
@@ -22,6 +22,45 @@ $("#login-form").submit(function (evt) {
     },
     success: function (response) {
       console.log("SUCCESS!!!!", response);
+      window.location.href = "userhome.html?username=" + user_login;
+    },
+    error: function (data) {
+      console.log(data["responseJSON"]);
+      alert(data["responseJSON"]["msg"]);
+    },
+  });
+});
+
+$("#register-form").submit(function (evt) {
+  evt.preventDefault();
+  // var form = $(this);
+  var userReg = document.getElementById("user-reg").value;
+  var passReg = document.getElementById("pass-reg").value;
+  var emailReg = document.getElementById("email-reg").value;
+  var cityReg = document.getElementById("city-reg").value;
+  var stateReg = document.getElementById("state-reg").value;
+  var countryReg = document.getElementById("country-reg").value;
+  var profileReg = document.getElementById("profile-reg").value;
+  //console.log("User Login:", user_login);
+  //console.log("Pass Login:", pass_login);
+  url = "http://127.0.0.1:5500/users";
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: JSON.stringify({
+      user_reg: userReg,
+      pass_reg: passReg,
+      email_reg: emailReg,
+      city_reg: cityReg,
+      state_reg: stateReg,
+      country_reg: countryReg,
+      profile_reg: profileReg,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    success: function (response) {
+      console.log("SUCCESS!!!!", response);
       window.location.replace("./page2.html");
     },
     error: function (data) {
