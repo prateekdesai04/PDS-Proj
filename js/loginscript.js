806f0b1038574ac71854b2a5174d6b42a70bb245
Rajat Raghuwanshi <77507423+rajat10cube@users.noreply.github.com>
2022-05-11 22:00:22 -0400

    login done

--- js/loginscript.js
+++ js/loginscript.js
@@ -1,37 +1,32 @@
-$('.message a').click(function () {
-  $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+$(".message a").click(function () {
+  $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
 });
 
 var loginbtn = document.getElementById("loginbtn");
 
-loginbtn.addEventListener("click", function (evt){
-	
-	evt.preventDefault();
-	
-	var user_login = document.getElementById('user-login').value;
-	var pass_login = document.getElementById('pass-login').value;
-	console.log('User Login:', user_login);
-	console.log('Pass Login:', pass_login);
-
-	window.location.replace("./page2.html");
-
-  url =
-    'http://127.0.0.1:5500/login'
+// loginbtn.addEventListener("click", function (evt) {
+$("#login-form").submit(function (evt) {
+  evt.preventDefault();
+  // var form = $(this);
+  var user_login = document.getElementById("user-login").value;
+  var pass_login = document.getElementById("pass-login").value;
+  console.log("User Login:", user_login);
+  console.log("Pass Login:", pass_login);
+  url = "http://127.0.0.1:5500/login";
   $.ajax({
     url: url,
-    type: 'POST',
-	data:{usename: user_login, password: pass_login},
-    datatype: 'json',
-    cors: 'true',
+    type: "POST",
+    data: JSON.stringify({ username: user_login, password: pass_login }),
+    headers: {
+      "Content-Type": "application/json",
+    },
     success: function (response) {
-		
-		console.log('SUCCESS!!!!', response);
+      console.log("SUCCESS!!!!", response);
+      window.location.replace("./page2.html");
     },
     error: function (data) {
-      console.log(data);
-      alert('Something went wrong, please try again.');
+      console.log(data["responseJSON"]);
+      alert(data["responseJSON"]["msg"]);
     },
   });
-
-}) 
-	
+});
